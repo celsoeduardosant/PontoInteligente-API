80788c33e5fb510efdc8516f817cd2346adce833
Celso Eduardo Silva De Santana <C00010220@cassi.com.br>
2020-03-16 16:12:44 -0300

    Adicionando EhCache

--- src/main/java/cassi/com/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/cassi/com/pontointeligente/api/PontoInteligenteApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, MailSenderAutoConfiguration.class})
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
